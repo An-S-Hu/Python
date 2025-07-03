1179a451d299515ba6c3c9b5d54f717222177ebc
An-S-Hu <bulletfloanshu@gmail.com>
2025-07-03 21:24:42 +0545

    lab 1

---
+++ first.py
@@ -0,0 +1,19 @@
+ #find the square root of a number
+# import math
+
+# num = float(input("Enter a number: "))
+
+# if num < 0:
+#     print("Square root of a negative number is not real.")
+# else:
+#     sqrt = math.sqrt(num)
+#     print(f"The square root of {num} is {sqrt}")
+
+
+ #Calculate the area of a circle
+# import math
+# radius = float(input("Enter the radius of the circle: "))
+# area = math.pi * (radius ** 2)
+# print(f"The area of the circle with radius {radius} is {area}")
+
+
